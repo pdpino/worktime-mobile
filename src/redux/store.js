55353cd6e47a2af92ec98fe526401253ee58e2bb
Pablo Pino <pdpino@uc.cl>
2018-08-12 16:05:51 -0300

    Configure redux-offline to persist data

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,6 +1,11 @@
 import { createStore } from 'redux';
+import { offline } from '@redux-offline/redux-offline';
+import offlineConfig from '@redux-offline/redux-offline/lib/defaults';
 import rootReducer from './reducers';
 
-const store = createStore(rootReducer);
+const store = createStore(
+  rootReducer,
+  offline(offlineConfig),
+);
 
 export default store;
