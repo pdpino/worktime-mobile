bf733e6cb2269287f2306255f402c6f60dd071cf
Pablo Pino <pdpino@uc.cl>
2018-08-15 23:36:40 -0300

    Add moment and redux-thunk

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,15 @@
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { offline } from '@redux-offline/redux-offline';
 import offlineConfig from '@redux-offline/redux-offline/lib/defaults';
+import thunk from 'redux-thunk';
 import rootReducer from './reducers';
 
 const store = createStore(
   rootReducer,
-  offline(offlineConfig),
+  compose(
+    applyMiddleware(thunk),
+    offline(offlineConfig),
+  ),
 );
 
 export default store;
