ee62d1a7c98acf8fffec14d9f6e9d4ff75cd685a
Pablo Pino <pdpino@uc.cl>
2018-08-16 21:04:23 -0300

    Add order for subjects

--- src/redux/selectors/subjects.js
+++ src/redux/selectors/subjects.js
@@ -3,5 +3,6 @@ import { createOrmSelector, ormSessionSelector } from './orm';
 // eslint-disable-next-line import/prefer-default-export
 export const subjectsSelector = createOrmSelector(
   ormSessionSelector,
-  ormSession => ormSession.Subject.all().toModelArray(),
+  ormSession => ormSession.Subject.all().toModelArray()
+    .sort((subj1, subj2) => (subj1.name <= subj2.name ? -1 : 1)),
 );
