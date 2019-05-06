16fdc11b9786fe7a2a63981090eb823a2f7cf538
Pablo Pino <pdpino@uc.cl>
2019-05-05 22:25:32 -0400

    Install react-native-fs, remove Buffer

--- android/app/src/main/java/com/worktime/MainApplication.java
+++ android/app/src/main/java/com/worktime/MainApplication.java
@@ -3,6 +3,7 @@ package com.worktime;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.rnfs.RNFSPackage;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
 import com.learnium.RNDeviceInfo.RNDeviceInfo;
 import com.oblador.vectoricons.VectorIconsPackage;
@@ -28,6 +29,7 @@ public class MainApplication extends Application implements ShareApplication, Re
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNFSPackage(),
             new ReactNativePushNotificationPackage(),
             new RNDeviceInfo(),
             new VectorIconsPackage(),
