9dc191ef3c198058e8bd23d2ec8a85a6c934be72
Pablo Pino <pdpino@uc.cl>
2019-05-08 20:42:34 -0400

    Refactor timeStats to isolate setTimeout hack

--- src/shared/timeCalculators/index.js
+++ src/shared/timeCalculators/index.js
@@ -1,9 +1,13 @@
-import {
-  sumTimesCalc, sumSubjectTimesCalc, getEmptyStats,
-} from './calculators';
+import { makeFunctionAsync } from '../utils';
+import TimeStats from './TimeStats';
 
-export {
-  sumTimesCalc,
-  sumSubjectTimesCalc,
-  getEmptyStats,
-};
+const calculatorCreator = action => makeFunctionAsync((...params) => {
+  const timeStats = new TimeStats();
+  timeStats[action](...params);
+  return timeStats.getStats();
+});
+
+export const sumTimesCalc = calculatorCreator('sumTimes');
+export const sumSubjectTimesCalc = calculatorCreator('sumSubjectTimes');
+
+export const getEmptyStats = () => (new TimeStats()).getStats();
