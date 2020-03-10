24c75c5796329fcfbd74c8f3dab2b4b1532b2eb8
Pablo Pino <pdpino@uc.cl>
2020-03-10 19:27:31 -0300

    Install i18n-js and rn-localize libs

--- android/app/src/main/java/com/worktime/MainApplication.java
+++ android/app/src/main/java/com/worktime/MainApplication.java
@@ -3,6 +3,7 @@ package com.worktime;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.reactcommunity.rnlocalize.RNLocalizePackage;
 import ui.fileselector.RNFileSelectorPackage;
 import com.rnfs.RNFSPackage;
 import com.dieam.reactnativepushnotification.ReactNativePushNotificationPackage;
@@ -30,6 +31,7 @@ public class MainApplication extends Application implements ShareApplication, Re
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+            new RNLocalizePackage(),
             new RNFileSelectorPackage(),
             new RNFSPackage(),
             new ReactNativePushNotificationPackage(),
