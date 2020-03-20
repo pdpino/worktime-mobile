fb3758189224d45ba5a721915a420252edc89e83
Pablo Pino <pdpino@uc.cl>
2020-03-20 17:18:26 -0300

    Add postBuild script to git

---
+++ postBuild.sh
@@ -0,0 +1,14 @@
+# REVIEW: maybe gradle is better for this?
+VERSION=$(cat package.json \
+  | grep version \
+  | head -1 \
+  | awk -F: '{ print $2 }' \
+  | sed 's/[",]//g')
+
+WORKING_DIR=$(pwd)
+APK_FNAME="${WORKING_DIR}/android/app/build/outputs/apk/release/app-release.apk"
+
+# Send through telegram
+echo tg-send-file ${APK_FNAME} -t "worktime v${VERSION}"
+
+# TODO: upload release to github
