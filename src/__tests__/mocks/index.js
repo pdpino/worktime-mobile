fcf9dfb7e689afe60dafface1f834a36376dfc5a
Pablo Pino <pdpino@uc.cl>
2020-08-26 14:45:51 -0400

    Add mock for react-native-push-notification

--- src/__tests__/mocks/index.js
+++ src/__tests__/mocks/index.js
@@ -3,7 +3,8 @@ import './netinfo';
 import './react-native-device-info';
 import './react-native-fs';
 import './react-native-localize';
-import './react-native-share';
+import './react-native-push-notification';
 import './react-native-reanimated';
+import './react-native-share';
 
 jest.useFakeTimers();
